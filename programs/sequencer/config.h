a82658b588372121a1375685ee9ecac0fb124afe
azzar <azzar.mr.zs@gmail.com>
2022-04-12 14:52:27 +0700

    Update config.h

--- programs/sequencer/config.h
+++ programs/sequencer/config.h
@@ -1,9 +1,9 @@
-#define Relay1            9
-#define Relay2            10
-#define Relay3            11
-#define Relay4            12
+#define Relay1            0  //D3
+#define Relay2            2  //D4
+#define Relay3            14 //D5
+#define Relay4            12 //D6
 
-#define push              8
+#define push              13 //D7
 
 int sensorPin = A0;
 int sensorValue = 0;
